5223d7a69a7814236f179cd5e7ca2aedd1499708
Adam Ahmed <avahmed@ualberta.ca>
2023-03-31 09:12:16 -0600

    allow selection of date

--- ui/src/components/Calendar/BasicDateCalendar.js
+++ ui/src/components/Calendar/BasicDateCalendar.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
+import dayjs from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
 export default function BasicDateCalendar() {
+  const [value, setValue] = React.useState(dayjs('2023-01-01'));
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar />
+      <DateCalendar value={value} onChange={(newValue) => { console.debug('Choose date ', newValue); setValue(newValue) }} />
     </LocalizationProvider>
   );
 }
