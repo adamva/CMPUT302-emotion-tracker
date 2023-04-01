40fbc921fd7e0cee80addb88ca63d94018405bc4
Adam Ahmed <avahmed@ualberta.ca>
2023-04-01 13:54:02 -0600

    add better layout

--- ui/src/components/Graph/BasicGraph.js
+++ ui/src/components/Graph/BasicGraph.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Box from '@mui/material/Box';
 import { Area, CartesianGrid, ComposedChart, Line, XAxis, YAxis } from 'recharts';
-import { Card, Button, FormControl, FormHelperText, MenuItem, Select, InputLabel, Typography, CardContent, Paper } from '@mui/material';
+import { Button, Card, CardContent, FormControl, FormHelperText, MenuItem, Select, InputLabel, Typography, Paper, Grid } from '@mui/material';
 
 import { emotions } from '../consts';
 import { fakeGraphData } from './fakeGraphData';
@@ -49,34 +49,48 @@ const BasicGraph = () => {
     }
   }
   return (
-    <Paper>
-      <Typography variant='h5'>Graph</Typography>
-      <ComposedChart 
-        width={100} 
-        height={100} 
-        data={fakeGraphData} 
-      >
-        {emotionList.map((emotion) => (renderEmotionGraphData(emotion)))}
-        <CartesianGrid stroke='#000' strokeDasharray="1 20"/>
-        <XAxis dataKey='name'/>
-        <YAxis />
-      </ComposedChart>
-      <FormControl sx={{ m: 1 }}>
-        <InputLabel id="active-emotion-select-helper-label">Emotion</InputLabel>
-        <Select
-          labelId="active-emotion-select-helper-label"
-          id="active-emotion-select-helper"
-          value={activeEmotion}
-          label="Active Emotion"
-          onChange={handelActiveEmotionChange}
-        >
-          {emotionList.map((emotion) => (
-            <MenuItem value={emotions[emotion].value}>{emotions[emotion].label}</MenuItem>
-          ))}
-        </Select>
-        <FormHelperText>Select the emotion to highlight</FormHelperText>
-      </FormControl>
-    </Paper>
+    <Grid container spacing={2} sx={{ backgroundColor: 'whitesmoke' }}>
+
+      <Grid item xs={12}>
+        <Paper>
+          <Typography variant='h5'>Graph</Typography>
+          <ComposedChart 
+            width={graphWidth} 
+            height={graphHeight} 
+            data={fakeGraphData} 
+            margin={
+              {
+                right: 50
+              }
+            }
+          >
+            {emotionList.map((emotion) => (renderEmotionGraphData(emotion)))}
+            <CartesianGrid stroke='#000' strokeDasharray="1 20"/>
+            <XAxis dataKey='name'/>
+            <YAxis />
+          </ComposedChart>
+        </Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper>
+          <FormControl sx={{ m: 1 }}>
+            <InputLabel id="active-emotion-select-helper-label">Emotion</InputLabel>
+            <Select
+              labelId="active-emotion-select-helper-label"
+              id="active-emotion-select-helper"
+              value={activeEmotion}
+              label="Active Emotion"
+              onChange={handelActiveEmotionChange}
+            >
+              {emotionList.map((emotion) => (
+                <MenuItem value={emotions[emotion].value}>{emotions[emotion].label}</MenuItem>
+              ))}
+            </Select>
+            <FormHelperText>Select the emotion to highlight</FormHelperText>
+          </FormControl>
+        </Paper>
+      </Grid>
+    </Grid>
   )
 }
 
