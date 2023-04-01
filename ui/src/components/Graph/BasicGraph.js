0d5798c01c4bd619c595ca92e87b37b7212b0de7
Adam Ahmed <avahmed@ualberta.ca>
2023-04-01 15:33:57 -0600

    add ability to select location

--- ui/src/components/Graph/BasicGraph.js
+++ ui/src/components/Graph/BasicGraph.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import Box from '@mui/material/Box';
-import { Area, CartesianGrid, ComposedChart, Line, XAxis, YAxis } from 'recharts';
+import { Area, CartesianGrid, ComposedChart, Line, ReferenceArea, XAxis, YAxis } from 'recharts';
 import { Button, Card, CardContent, FormControl, FormHelperText, MenuItem, Select, InputLabel, Typography, Paper, Grid } from '@mui/material';
 
 import { emotions } from '../consts';
-import { fakeGraphData } from './fakeGraphData';
+import { fakeGraphData, fakeGraphDataLocation } from './fakeGraphData';
 import MultipleSelectChip from './EmotionListSelect';
 
 const BasicGraph = () => {
@@ -14,8 +14,14 @@ const BasicGraph = () => {
 
   const [ activeEmotion, setActiveEmotion ] = useState('tired');
   const [ activeEmotionList, setActiveEmotionList ] = useState(['tired', 'anger'])
+  const [ activeLocation, setActiveLocation ] = useState('library');
 
-  const handelActiveEmotionChange = (e) => {
+  const handleActiveLocationChange = (e) => {
+    let newActiveLocation = e.target.value;
+    setActiveLocation(newActiveLocation);
+  }
+
+  const handleActiveEmotionChange = (e) => {
     let newActiveEmotion = e.target.value;
     // Rearrange the graph to have the active emotion at the beginning of the list such that
     // the graph will render the non-active emotions ontop
@@ -72,20 +78,23 @@ const BasicGraph = () => {
             <CartesianGrid horizontal={false} stroke='#000' strokeDasharray="1 20"/>
             <XAxis dataKey='name'/>
             <YAxis label={{ value: 'Emotion Level', angle: -90, position: 'insideLeft' }} />
+            {activeLocation && 
+              <ReferenceArea x1={fakeGraphDataLocation[activeLocation].x1} x2={fakeGraphDataLocation[activeLocation].x2}/>
+            }
           </ComposedChart>
         </Paper>
       </Grid>
       <Grid item xs={12}>
         <Paper sx={{ marginLeft: 2, marginRight: 2 }}>
           <Typography variant='h5' color='primary' sx={{ m: 1 }}>Graph Controls</Typography>
-          <FormControl sx={{ m: 1 }}>
+          <FormControl sx={{ m: 1, maxWidth:'60%' }}>
             <InputLabel id="active-emotion-select-helper-label">Active Emotion</InputLabel>
             <Select
               labelId="active-emotion-select-helper-label"
               id="active-emotion-select-helper"
               value={activeEmotion}
               label="Active Emotion"
-              onChange={handelActiveEmotionChange}
+              onChange={handleActiveEmotionChange}
             >
               {activeEmotionList.map((emotion) => (
                 <MenuItem value={emotions[emotion].value}>{emotions[emotion].label}</MenuItem>
@@ -94,6 +103,22 @@ const BasicGraph = () => {
             <FormHelperText>Select the emotion to highlight</FormHelperText>
           </FormControl>
           <MultipleSelectChip activeEmotionList={activeEmotionList} setActiveEmotionList={setActiveEmotionList} />
+          <FormControl sx={{ m: 1, maxWidth:'60%' }}>
+            <InputLabel id="active-location-select-helper-label">Active Location</InputLabel>
+            <Select
+              labelId="active-location-select-helper-label"
+              id="active-location-select-helper"
+              value={activeLocation}
+              label="Active Location"
+              onChange={handleActiveLocationChange}
+            >
+              <MenuItem value=""><em>None</em></MenuItem>
+              {Object.keys(fakeGraphDataLocation).map((location) => (
+                <MenuItem value={location}>{fakeGraphDataLocation[location].label}</MenuItem>
+              ))}
+            </Select>
+            <FormHelperText>Select the location to highlight</FormHelperText>
+          </FormControl>
         </Paper>
       </Grid>
     </Grid>
