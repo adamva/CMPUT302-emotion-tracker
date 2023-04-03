9cd568993729980cd79f26eb41f018ab9a1688ad
Adam Ahmed <avahmed@ualberta.ca>
2023-04-03 09:26:34 -0600

    add expansion back randomly

--- ui/src/components/Tips/TipsCard.js
+++ ui/src/components/Tips/TipsCard.js
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import { Link as ReactRouterDomLink } from 'react-router-dom';
+
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Avatar from '@mui/material/Avatar';
+import Collapse from '@mui/material/Collapse';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import ShareMenu from './ShareMenu';
 import { getRandomNumber } from '../../utils/Utils';
@@ -18,10 +23,28 @@ Example code here
 https://mui.com/material-ui/react-card/#media
 */
 
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
 export default function TipsCard({ emotion, content }) {
+    const [expanded, setExpanded] = React.useState(false);
+    const disableComments = getRandomNumber(0, 1) ? true : false;
     console.debug('Rendering emotion TipsCard content:');
     console.debug(content);
     const learnMoreURL = `https://www.google.com/search?q=${emotion.label}`;
+    
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
+
   return (
     <Card sx={{ maxWidth: 345 }}>
         {content.imageSrc !== '' && 
@@ -50,7 +73,32 @@ export default function TipsCard({ emotion, content }) {
       <CardActions>
         <ShareMenu />
         <Button component={ReactRouterDomLink} to={learnMoreURL} size="small">Learn More</Button>
+        { !disableComments &&
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        }
       </CardActions>
+      { !disableComments &&
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do iusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Typography>
+            <Typography paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do iusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Typography>
+            <Typography paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do iusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      }
     </Card>
   );
 }
