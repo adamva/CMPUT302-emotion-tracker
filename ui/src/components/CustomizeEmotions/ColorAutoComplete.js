86ed6f859b613f8c44a4acf85d9aa1febb102366
Adam Ahmed <avahmed@ualberta.ca>
2023-04-04 12:27:18 -0600

    cannot clear selection

--- ui/src/components/CustomizeEmotions/ColorAutoComplete.js
+++ ui/src/components/CustomizeEmotions/ColorAutoComplete.js
@@ -24,6 +24,7 @@ const ColorAutoComplete = ({ onChange, emotionValue, currentColorValue }) => {
         id={`${emotionValue}-color-select`}
         options={colorSwatch}
         autoHighlight
+        disableClearable
         value={value}
         onChange={(event, newValue) => {
             if (newValue) {
