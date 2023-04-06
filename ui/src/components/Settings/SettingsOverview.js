886fad9b964ad105d455b3a107522c79507bacc2
Adam Ahmed <avahmed@ualberta.ca>
2023-04-06 08:31:20 -0600

    finish up settings page

--- ui/src/components/Settings/SettingsOverview.js
+++ ui/src/components/Settings/SettingsOverview.js
@@ -6,6 +6,7 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import { Typography, Toolbar, AppBar, Button } from '@mui/material';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 const SettingsOverview = () => {
     const navigate = useNavigate();
@@ -22,14 +23,44 @@ const SettingsOverview = () => {
         <Paper sx={{ m: 2, p: 2 }}>
             <Grid container spacing={2} justifyContent="center" alignItems="center">
                 <Grid item xs={12}><Typography variant='h5' color='primary'>Accessibility</Typography></Grid>
-                <Grid item xs={12}><Button variant='contained' fullWidth>Language</Button></Grid>
-                <Grid item xs={12}><Button variant='contained' fullWidth>Voice Mode</Button></Grid>
+                <Grid item xs={12}>
+                    <Button 
+                    variant='contained' 
+                    fullWidth
+                    onClick={() => navigate('/settings/language')}
+                    endIcon={<NavigateNextIcon />}
+                    sx={{display: 'flex', justifyContent: 'space-between'}}
+                    >
+                        Language
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button 
+                    variant='contained' 
+                    fullWidth
+                    onClick={() => navigate('/settings/voice-mode')}
+                    endIcon={<NavigateNextIcon />}
+                    sx={{display: 'flex', justifyContent: 'space-between'}}
+                    >
+                        Voice Mode
+                    </Button>
+                </Grid>
             </Grid>
         </Paper>
         <Paper sx={{ m: 2, p: 2 }}>
             <Grid container spacing={2} justifyContent="center" alignItems="center">
                 <Grid item xs={12}><Typography variant='h5' color='primary'>Customize</Typography></Grid>
-                <Grid item xs={12}><Button variant='contained' fullWidth onClick={() => navigate('/settings/customize')}>Emotion Color Swatch</Button></Grid>
+                <Grid item xs={12}>
+                    <Button 
+                    variant='contained' 
+                    fullWidth
+                    onClick={() => navigate('/settings/customize')}
+                    endIcon={<NavigateNextIcon />}
+                    sx={{display: 'flex', justifyContent: 'space-between'}}
+                    >
+                        Emotion Color Swatch
+                    </Button>
+                </Grid>
             </Grid>
         </Paper>
         </Box>
